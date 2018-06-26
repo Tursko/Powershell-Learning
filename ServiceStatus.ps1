c9c199b7ed22186e5dc4ba42440442494b854c57
Taylor Riley <taylor@stykocompany.com>
2018-06-26 15:19:24 -0500

    Service Status Script
    Gives total of services that are running and stopped

---
+++ ServiceStatus.ps1
@@ -0,0 +1,19 @@
+﻿$services = Get-Service
+$countR = 0
+$countS = 0
+$countTotal = 0
+
+ForEach ($item in $services) {
+    if($item.Status -eq 'Running') {
+        $countR++        
+    }
+    else {
+        $countS++
+    }
+    $countTotal++
+
+}
+
+Write-Host "Total Services Running" $countR -foregroundColor green
+Write-Host "Total Services Stopped" $countS -BackgroundColor red
+Write-Host "Total Services" $countTotal
