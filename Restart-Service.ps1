035fa7fdb10a26ff4d02e29f77b5cb8994386a80
Taylor Riley <taylor@stykocompany.com>
2018-07-02 14:09:00 -0500

    Update Restart-Service.ps1

--- Restart-Service.ps1
+++ Restart-Service.ps1
@@ -1,5 +1,5 @@
 #Name of the service we want to kill and restart
-#change this to whatever service-name you want. 
+#change this to whatever servicename you want. 
 #Please check the service name by typing 'Get-Service' on your own Powershell instance
 Param(
     [string]$serviceName = 'AudioSrv'
@@ -12,10 +12,11 @@ ForEach ($item in $services) {
     if($item.Name -eq $serviceName) {
         Write-Host $serviceName "process has been FOUND" -ForegroundColor green
 
-        Stop-Service $item
-
-        #Restart-Service
-        if($item.Status -eq "Stopped") {
+        if($item.Status -eq "Running") {
+            Write-Host $item "process is currently RUNNING"
+            Stop-Service $item
+        }
+        if($item.Status -eq "Stopped") { #I check this regardless
             Write-Host $item "process has been STOPPED" -ForegroundColor Red
             
             Start-Service $item
@@ -29,7 +30,7 @@ ForEach ($item in $services) {
             }
 
         } else {
-            Write-Host "The service" $item "did not STOP, are you running the script as an Admin?" -ForegroundColor red
+            Write-Host "The service" $item "did not STOP, do you have the correct permissions?" -ForegroundColor red
         }
         $count++
         break
