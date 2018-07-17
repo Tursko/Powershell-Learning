ea2b08bc89dc16e5475f7254cdb9784752921ea3
Taylor Riley <taylor@stykocompany.com>
2018-07-17 15:45:19 -0500

    Comments

--- Rename-InvalidFiles.ps1
+++ Rename-InvalidFiles.ps1
@@ -1,5 +1,5 @@
-#Author: Taylor Riley (Tursko)
-#http://github.com/Tursko
+# Author: Taylor Riley (Tursko)
+# http://github.com/Tursko
 
 Param (
     $path = 'C:\Users\t\Desktop\DateData'
@@ -8,19 +8,18 @@ Write-Host "Indexing $path" -ForegroundColor Blue
 $fileListMain = Get-ChildItem -Path $path -Recurse 
 
 #100% functional!
-#This function takes a list of files
-#And will only rename files, folders will be excluded.
+# This function takes a list of files
+# And will ONLY rename files, FOLDERS ARE EXCLUDED. 
 function RenameFilesOnly($fileList) {
     Write-Host "Renaming Files Only...." -ForegroundColor Green
     Write-Host "Searching dir:" $path
     ForEach ($fileName in $fileList) {
-        #We don't care about the actual file names. 
-        if($fileName.Attributes -match 'Directory') {
+        if($fileName.Attributes -match 'Directory') { # Avoiding directories
             continue
         }else {
-            if($fileName.Name[0] -ne '.') {
+            if($fileName.Name[0] -ne '.') { # Avoiding certain hidden files
                 $fixedName = $fileName.Name
-
+                # Below are the fields we are correcting
                 $fixedName = $fixedName.Replace("#","Nos")
                 $fixedName = $fixedName.Replace("~","")
                 $fixedName = $fixedName.Replace("&"," and ")
@@ -30,6 +29,7 @@ function RenameFilesOnly($fileList) {
                 $fixedname = $fixedName -replace '([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9])', '$1-$2-$3'
                 $fixedname = $fixedName -replace '([0-9])\.([0-9][0-9])\.([0-9][0-9])', '0$1-$2-$3'
                 $fixedname = $fixedName -replace '([0-9])\.([0-9])\.([0-9][0-9])', '0$1-0$2-$3'
+                #Now renaming the file
                 Rename-Item -LiteralPath $fileName.FullName $fixedName
            
             }
